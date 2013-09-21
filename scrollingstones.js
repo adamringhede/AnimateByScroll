11b0d6302864636b5329969027b4e212b0fb81f7
Adam <adamringhede@live.com>
2013-09-21 14:08:52 +0200

    Added comments

--- scrollingstones.js
+++ scrollingstones.js
@@ -1,16 +1,18 @@
 
 
 (function(){
-	var self = this;
 	var eventObjects = [];
 	var disabled = false;
-	var pub = {		/*element, styles, startAt, endAt */
+	var pub = {		
+		/** Add an element that should be animated on scroll
+		 * parameters: arg.element, arg.styles, arg.startAt, arg.endAt 
+		 */
 		add: function( arg ){
 			// Add styles for the object
 			var tS = {};
 			for(var i in arg.styles) {
 				tS[i] = [/*default*/ parseInt(arg.element.style[i]) ,/*new*/ parseInt(arg.styles[i]) ];
-			};
+			}
 			// Add eventObject to datastructure
 			eventObjects.push({
 				animationStartAt: arg.startAt,
@@ -21,9 +23,13 @@
 		clear: function(){
 			console.log("not yet implemented");
 		},
+		/** Disable the animations
+		 */
 		disable: function(){
 			disabled = true;
 		},
+		/** Enable the animations
+		 */
 		enable: function(){
 			disabled = false;
 		}
@@ -31,11 +37,15 @@
 	
 	$(document).scroll(function(){
 		if(disabled) return;
-		var sT = $(document).scrollTop(
+		var sT = $(document).scrollTop();		
 		for(var i = 0; i < eventObjects.length; i++){
 			var eo = eventObjects[i];
 			if(sT <= eo.animationEndAt && sT >= eo.animationStartAt) {
-				
+				var progress = (sT - eo.animationStartAt) / (eo.animationEndAt - eo.animationStartAt);
+				for (var attr in eo.targetStyle) {
+					var def = eo.targetStyle[attr][0];
+					eo.element.style[attr] = def+((eo.targetStyle[attr][1]-def)*progress)
+				}
 			}
 		}
 	});
