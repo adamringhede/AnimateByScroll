289adaae5b8a2e411d4f0d50d29cba69ccf01d84
Adam <adamringhede@live.com>
2013-09-21 13:39:50 +0200

    Added stuff

--- scrollingstones.js
+++ scrollingstones.js
@@ -1,10 +1,43 @@
-function stone(element, newStyle, startAt, endAt) {
-	var st = $(document).scrollTop(),
-		old = element.css(newStyle[0]),
-		newVal = element.css(newStyle[1]);
+
+
+(function(){
+	var self = this;
+	var eventObjects = [];
+	var pub = {
+		add: function(){
+			eventObjects.push({
+				endAt:
+				startAt:
+				
+			});
+		},
+		clear: function(){
+			console.log("not yet implemented");
+		}
+	};
+	
+	
 	$(document).scroll(function(){
-		if(st >= endAt && st <= startAt) {
-			
+		var sT = $(document).scrollTop(
+		for(var i = 0; i < eventObjects.length; i++){
+			var eo = eventObjects[i];
+			if(sT <= eo.endAt && sT >= eo.startAt) {
+				
+			}
 		}
 	});
+	
+	window.ScrollingStones = pub;
+})();
+/*
+function stone(element, newStyle, startAt, endAt) {
+	var 
+		old = element.css(newStyle[0]),
+		newVal = element.css(newStyle[1]);
+		
+	
 }
+
+
+stone(someEl, {left:100, top:40}, 400, 700);
+*/
